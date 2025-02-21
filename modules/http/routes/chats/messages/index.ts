ab0fc8010979e86d382e3b763d677b4bcb45ab14
ftovar8 <felix.godo@gmail.com>
2025-02-21 16:49:24 -0300

    chore: clean code

--- modules/http/routes/chats/messages/index.ts
+++ modules/http/routes/chats/messages/index.ts
@@ -1,11 +1,5 @@
-import { Agent } from '@aimpact/agents-api/business/agent';
-import { Chat } from '@aimpact/agents-api/business/chats';
-import type { IChatData } from '@aimpact/agents-api/data/interfaces';
-import { ErrorGenerator } from '@aimpact/agents-api/http/errors';
-import type { IAuthenticatedRequest } from '@aimpact/agents-api/http/middleware';
 import { UserMiddlewareHandler } from '@aimpact/agents-api/http/middleware';
-import { Response } from '@beyond-js/response/main';
-import type { Application, Response as IResponse } from 'express';
+import type { Application } from 'express';
 import { audio } from './audio';
 import { v2 } from './v2';
 
@@ -30,54 +24,54 @@ export class ChatMessagesRoutes {
 		app.post('/chats/:id/messages/v2', UserMiddlewareHandler.validate, v2);
 	}
 
-	static async sendMessage(req: IAuthenticatedRequest, res: IResponse) {
-		const { test } = req.query;
-		if (!!test) {
-			return res.status(400).json(new Response({ error: ErrorGenerator.testingError() }));
-		}
+	// static async sendMessage(req: IAuthenticatedRequest, res: IResponse) {
+	// 	const { test } = req.query;
+	// 	if (!!test) {
+	// 		return res.status(400).json(new Response({ error: ErrorGenerator.testingError() }));
+	// 	}
 
-		const chatId = req.params.id;
-		if (!chatId) return res.status(400).json({ status: false, error: 'Parameter chatId is required' });
+	// 	const chatId = req.params.id;
+	// 	if (!chatId) return res.status(400).json({ status: false, error: 'Parameter chatId is required' });
 
-		let chat: IChatData;
-		try {
-			const response = await Chat.get(chatId, 'false');
-			if (response.error) return res.status(400).json({ status: false, error: response.error });
-			chat = response.data;
-		} catch (exc) {
-			res.json({ status: false, error: exc.message });
-		}
+	// 	let chat: IChatData;
+	// 	try {
+	// 		const response = await Chat.get(chatId, 'false');
+	// 		if (response.error) return res.status(400).json({ status: false, error: response.error });
+	// 		chat = response.data;
+	// 	} catch (exc) {
+	// 		res.json({ status: false, error: exc.message });
+	// 	}
 
-		const done = (specs: { status: boolean; error?: IError }) => {
-			const { status, error } = specs;
-			res.write('ÿ');
-			res.write(JSON.stringify({ status, error }));
-			res.end();
-		};
-		res.setHeader('Content-Type', 'text/plain');
-		res.setHeader('Transfer-Encoding', 'chunked');
+	// 	const done = (specs: { status: boolean; error?: IError }) => {
+	// 		const { status, error } = specs;
+	// 		res.write('ÿ');
+	// 		res.write(JSON.stringify({ status, error }));
+	// 		res.end();
+	// 	};
+	// 	res.setHeader('Content-Type', 'text/plain');
+	// 	res.setHeader('Transfer-Encoding', 'chunked');
 
-		const { user } = req;
-		let metadata: IMetadata;
-		try {
-			const { iterator, error } = await Agent.sendMessage(chatId, req.body, user.uid);
-			if (error) return done({ status: false, error });
+	// 	const { user } = req;
+	// 	let metadata: IMetadata;
+	// 	try {
+	// 		const { iterator, error } = await Agent.sendMessage(chatId, req.body, user.uid);
+	// 		if (error) return done({ status: false, error });
 
-			for await (const part of iterator) {
-				const { chunk } = part;
-				chunk && res.write(chunk);
-				if (part.metadata) {
-					metadata = part.metadata;
-					break;
-				}
-			}
-		} catch (exc) {
-			console.error(exc);
-			return done({ status: false, error: ErrorGenerator.internalError('HRC100') });
-		}
+	// 		for await (const part of iterator) {
+	// 			const { chunk } = part;
+	// 			chunk && res.write(chunk);
+	// 			if (part.metadata) {
+	// 				metadata = part.metadata;
+	// 				break;
+	// 			}
+	// 		}
+	// 	} catch (exc) {
+	// 		console.error(exc);
+	// 		return done({ status: false, error: ErrorGenerator.internalError('HRC100') });
+	// 	}
 
-		if (metadata?.error) return done({ status: false, error: metadata.error });
+	// 	if (metadata?.error) return done({ status: false, error: metadata.error });
 
-		done({ status: true });
-	}
+	// 	done({ status: true });
+	// }
 }
