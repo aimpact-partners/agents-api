d5fafe73e50974d8d2301f075abcff987ed14d78
ftovar8 <felix.godo@gmail.com>
2025-03-07 15:22:54 -0300

    feat: declarations

--- modules/ws/config/index.ts
+++ modules/ws/config/index.ts
@@ -1,12 +1,12 @@
-type Environments = 'development' | 'testing' | 'beta' | 'production' | 'local';
+type Environments = 'local' | 'development' | 'testing' | 'beta' | 'production';
 interface IEndpoint {
 	port?: number;
 	environment: Environments;
 }
 
 const environments: { [key in Environments]: string } = {
-	development: 'wss://dev.agents.api.aimpact.partners',
 	local: 'wss://dev.agents.api.aimpact.partners',
+	development: 'wss://dev.agents.api.aimpact.partners',
 	testing: 'wss://test.agents.api.aimpact.partners',
 	beta: 'wss://beta.agents.api.aimpact.partners',
 	production: 'wss://agents.api.aimpact.partners'
